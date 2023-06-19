6b6fd0920970636525bae305e7899cd839b5cc4f
Kenny <kennyvungo@gmail.com>
2023-06-19 14:14:31 -0700

    added dropdown

--- frontend/src/Components/Navigation/ProfileButton.js
+++ frontend/src/Components/Navigation/ProfileButton.js
@@ -1,11 +1,63 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useDispatch } from 'react-redux';
+import * as sessionActions from '../../store/session';
 
-const ProfileButton = () => {
-    return (
-        <div className = 'profbutton'>
-            <i class="fa-brands fa-facebook"></i>
-        </div>
-    )
+function ProfileButton({ user }) {
+  const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(false);
+  
+  const openMenu = () => {
+    if (showMenu) return;
+    setShowMenu(true);
+  };
+  
+  useEffect(() => {
+    if (!showMenu) return;
+
+    const closeMenu = () => {
+      setShowMenu(false);
+    };
+
+    document.addEventListener('click', closeMenu);
+  
+    return () => document.removeEventListener("click", closeMenu);
+  }, [showMenu]);
+
+  const logout = (e) => {
+    e.preventDefault();
+    dispatch(sessionActions.logout());
+  };
+
+  return (
+    <>
+      <div className = "profbutton" onClick={openMenu} >
+        <i class="fa-brands fa-facebook"></i>
+      </div>
+      {showMenu && (
+        <ul className="profile-dropdown">
+          <li>{user.firstName}</li>
+          <li>{user.lastName}</li>
+          <li>{user.email}</li>
+          <li>
+            <button onClick={logout}>Log Out</button>
+          </li>
+        </ul>
+      )}
+    </>
+  );
 }
 
-export default ProfileButton
+export default ProfileButton;
+
+
+
+
+
+
+
+
+
+
+// <div className = 'profbutton'>
+// <i class="fa-brands fa-facebook"></i>
+// </div>
--- frontend/src/Components/Navigation/index.js
+++ frontend/src/Components/Navigation/index.js
@@ -14,16 +14,15 @@ const sessionUser = useSelector(state => state.session.user);
 
     let sessionLinks;
     const dispatch = useDispatch();
-    const handleClick= (e) =>{
-        e.preventDefault();
-        dispatch(sessionActions.logout())
-        // console.log("LOGGED OUT")
-    }
+    // const handleClick= (e) =>{
+    //     e.preventDefault();
+    //     dispatch(sessionActions.logout())
+    //     // console.log("LOGGED OUT")
+    // }
     if (sessionUser) {
     sessionLinks = (
         <div className='navbar'>
         <ProfileButton user={sessionUser} />
-            <button onClick={handleClick}>Log Out</button>
         </div>
     );
     } else {
