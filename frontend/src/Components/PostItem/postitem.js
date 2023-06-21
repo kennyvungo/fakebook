329b9a97d1e30a1d65775ae9decb81e5474ee558
Kenny <kennyvungo@gmail.com>
2023-06-20 17:57:50 -0700

    added drop down menu to post

--- frontend/src/Components/PostItem/postitem.js
+++ frontend/src/Components/PostItem/postitem.js
@@ -4,17 +4,44 @@ import {useSelector } from 'react-redux';
 import { BiLike} from 'react-icons/bi'
 import {GoComment} from'react-icons/go'
 import {BsThreeDots} from 'react-icons/bs'
+import { useState,useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 const PostItem = ({post}) => {
     const sessionUser = useSelector(state => state.session.user)
+    const dispatch = useDispatch();
+    const [showMenu, setShowMenu] = useState(false);
+    const openMenu = () => {
+        if (showMenu) return;
+        setShowMenu(true);
+      }; 
+
+    useEffect(() => {
+        if (!showMenu) return;
+    
+        const closeMenu = () => {
+          setShowMenu(false);
+        };
+    
+        document.addEventListener('click', closeMenu);
+      
+        return () => document.removeEventListener("click", closeMenu);
+      }, [showMenu]);
+
   return (
     <div className="postbox">
         <div className='postUser'>
             {sessionUser.firstName}
             {sessionUser.lastName}
-            <div className='postdots'>
+            <div className='postdots' onClick={openMenu}>
                 <BsThreeDots/>
             </div>
         </div>
+        {showMenu &&(
+                <ul className="postdropdown">
+                    <ul>Edit Post</ul>
+                    <ul>Delete Post</ul>
+                </ul>
+            )}
         <div className='postbody'>
         {post.body}
         </div>
