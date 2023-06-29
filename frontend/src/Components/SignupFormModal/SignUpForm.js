e9af10aeb84959fd7c329452b728edae43f45414
Kenny <kennyvungo@gmail.com>
2023-06-28 17:27:09 -0700

    revamped signup styling

--- frontend/src/Components/SignupFormModal/SignUpForm.js
+++ frontend/src/Components/SignupFormModal/SignUpForm.js
@@ -8,11 +8,18 @@ const SignUpFormPage = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [first_name,setFirstName] = useState("");
+    const [gender,setGender] = useState("");
     const [last_name,setLastName] = useState("");
+    const [birthday,setBirthday] = useState();
     const [errors, setErrors] = useState([]);
-
-
+    const [year,setYear] = useState("");
+    const [day,setDay] = useState("");
+    const [month,setMonth] = useState("");
+    const days = [...Array(31).keys()];
+    const months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Oct","Nov","Dec"]
+    const years = [...Array(2023).keys()].reverse()
     const handleSubmit = (e) => {
+        setBirthday(`${year}-${month}-${day}`)
         e.preventDefault();
             setErrors([]);
             return dispatch(sessionActions.signup({ email, password,first_name,last_name}))
@@ -73,6 +80,45 @@ const SignUpFormPage = () => {
                 onChange={(e) => setPassword(e.target.value)}
                 required
                 />
+                <div className="birthday">
+                <select className="birth month" id="month" defaultValue= {"Jan"} onChange={e => setMonth(e.target.value)}
+                >
+                {months.map(month => <option>{month}</option>)}
+                </select>
+                <select className="birth" id="days" defaultValue={1} onChange={e => setDay(e.target.value)}>
+                {days.map(day => <option> {day}</option>)}
+                </select>
+                <select className="birth" id="year" defaultValue={2023} onChange={e => setYear(e.target.value)} >
+                    {years.map(year => <option>{year}</option>)}
+                </select>
+                </div>
+
+                <div className="gender">
+                    <label>
+                        <div className="genderitem">Female
+                        <input type="radio" name="gender" value="Female"
+                        onChange={(e) => setGender(e.target.value)} 
+                        />
+                        </div>
+                    </label>
+                    <label>
+                        <div className="genderitem">Male
+                        <input type="radio" name="gender" value="Male"
+                        onChange={(e) => setGender(e.target.value)} 
+                        />
+                        </div>
+                    </label>
+                    <label>
+                        <div className="genderitem">Custom
+
+                        <input type="radio" name="gender" value="Male"
+                        onChange={(e) => setGender(e.target.value)} 
+                        />
+                        </div>
+                    </label>
+                </div>
+
+
                 <div className="disclaim">
                 <p>People who use our service may have uploaded your contact information to Facebook.</p>
                 <a>Learn More</a>
