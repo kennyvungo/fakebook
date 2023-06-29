e78062c2bfeb5329d30c27e64d8a20e66af69a16
Kenny <kennyvungo@gmail.com>
2023-06-29 10:50:16 -0700

    fixed primary keys

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,12 @@ require "open-uri"
     Post.destroy_all
     puts "Resetting primary keys..."
     # For easy testing, so that after seeding, the first `User` has `id` of 1
-    ApplicationRecord.connection.reset_pk_sequence!('users','posts','comments','likes','pendingfriends','friends')
+    ApplicationRecord.connection.reset_pk_sequence!('users')
+    ApplicationRecord.connection.reset_pk_sequence!('posts')
+    ApplicationRecord.connection.reset_pk_sequence!('comments')
+    ApplicationRecord.connection.reset_pk_sequence!('likes')
+    ApplicationRecord.connection.reset_pk_sequence!('pendingfriends')
+    ApplicationRecord.connection.reset_pk_sequence!('friends')
   
     puts "Creating users..."
     # Create one user with an easy to remember username, email, and password:
