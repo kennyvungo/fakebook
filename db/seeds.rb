a771f9904b9fefdf69fc931e07b1f7e638de4325
Kenny <kennyvungo@gmail.com>
2023-06-29 10:45:46 -0700

    edited seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,10 +11,14 @@ require "open-uri"
     puts "Destroying tables..."
     # Unnecessary if using `rails db:seed:replant`
     User.destroy_all
-  
+    Comment.destroy_all
+    Like.destroy_all
+    Pendingfriend.destroy_all
+    Friend.destroy_all
+    Post.destroy_all
     puts "Resetting primary keys..."
     # For easy testing, so that after seeding, the first `User` has `id` of 1
-    ApplicationRecord.connection.reset_pk_sequence!('users')
+    ApplicationRecord.connection.reset_pk_sequence!('users','posts','comments','likes','pendingfriends','friends')
   
     puts "Creating users..."
     # Create one user with an easy to remember username, email, and password:
